87cd235f95625d37e6cc2c0fe21ba4412fd36ab5
Eikko_ <leo.poilane91@gmail.com>
2025-05-02 20:06:19 +0200

    Handling the choice of algo for max flow problem + implementing push-relabel call

--- OR Project/src/main.py
+++ OR Project/src/main.py
@@ -1,6 +1,7 @@
 import os
 from reader import read_capacity_matrix, read_capacity_and_cost_matrix, display_matrix
 from min_cost_flow import bellman_ford 
+from push_relabel import push_relabel
 
 PROPOSAL_DIR = os.path.join(os.path.dirname(__file__), '..', 'proposals')
 
@@ -43,6 +44,26 @@ def main():
                 print("\nDetection: MAXIMAL FLOW problem.")
                 cap = read_capacity_matrix(filepath)
                 display_matrix(cap, "Capacity Matrix")
+
+                while True:
+                    print("\nChoose the algorithm to perform:")
+                    print("1. Push-Relabel")
+                    print("2. FF")
+                    algorithm_choice = input("Enter the number of the algorithm (1 or 2): ")
+
+                    if algorithm_choice == "1":
+                        print("\nPush-Relabel algorithm result:")
+                        max_flow = push_relabel(cap, 0, len(cap) - 1)
+                        break
+                    elif algorithm_choice == "2":
+                        print("\nEdmonds-Karp algorithm result:")
+                        # max_flow = ff(cap, source, sink)
+                        max_flow = 0  # Placeholder for the actual FF algorithm implementation
+                        break
+                    else:
+                        print("❌ Invalid choice. Please enter 1 or 2.")
+                print(f"Maximum Flow: {max_flow}")
+
         except Exception as e:
             print(f"❌ Error during the analysis: {e}")
 
